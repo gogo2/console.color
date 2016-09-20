10b7d0716d0db2f0c69fa794c8a820aac02e427d
Bartek <barteks95@gmail.com>
2016-09-20 12:02:07 +0200

    constexpr

--- include/Win32Handler.h
+++ include/Win32Handler.h
@@ -21,11 +21,32 @@ namespace cc {
 
         static WORD getW32AttrWord(Color color, bool bright, bool bg);
 
-        static inline WORD zeroColor() { return 240; }
+        static constexpr WORD zeroColor() { return 240; }
 
-        static inline WORD zeroBGColor() { return 15; }
+        static constexpr WORD zeroBGColor() { return 15; }
 
-        static const WORD w32_attrs[];
+        static constexpr WORD w32_attrs[] = {
+                0,                                                      //0
+                FOREGROUND_RED,                                         //1
+                FOREGROUND_GREEN,                                       //2
+                FOREGROUND_RED | FOREGROUND_GREEN,                      //3
+                FOREGROUND_BLUE,                                        //4
+                FOREGROUND_RED | FOREGROUND_BLUE,                       //5
+                FOREGROUND_GREEN | FOREGROUND_BLUE,                     //6
+                FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,    //7
+
+                0,                                                      //8
+                BACKGROUND_RED,                                         //9
+                BACKGROUND_GREEN,                                       //10
+                BACKGROUND_RED | BACKGROUND_GREEN,                      //11
+                BACKGROUND_BLUE,                                        //12
+                BACKGROUND_RED | BACKGROUND_BLUE,                       //13
+                BACKGROUND_GREEN | BACKGROUND_BLUE,                     //14
+                BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE,    //15
+
+                FOREGROUND_INTENSITY,                                   //16
+                BACKGROUND_INTENSITY                                    //17
+        };
     };
 
 }
--- src/Win32Handler.cpp
+++ src/Win32Handler.cpp
@@ -11,29 +11,6 @@ namespace cc {
 
     const WORD Win32Handler::default_attrs = Win32Handler::getCurrentAttrs();
 
-    const WORD Win32Handler::w32_attrs[] = {
-            0,                                                      //0
-            FOREGROUND_RED,                                         //1
-            FOREGROUND_GREEN,                                       //2
-            FOREGROUND_RED | FOREGROUND_GREEN,                      //3
-            FOREGROUND_BLUE,                                        //4
-            FOREGROUND_RED | FOREGROUND_BLUE,                       //5
-            FOREGROUND_GREEN | FOREGROUND_BLUE,                     //6
-            FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,    //7
-
-            0,                                                      //8
-            BACKGROUND_RED,                                         //9
-            BACKGROUND_GREEN,                                       //10
-            BACKGROUND_RED | BACKGROUND_GREEN,                      //11
-            BACKGROUND_BLUE,                                        //12
-            BACKGROUND_RED | BACKGROUND_BLUE,                       //13
-            BACKGROUND_GREEN | BACKGROUND_BLUE,                     //14
-            BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE,    //15
-
-            FOREGROUND_INTENSITY,                                   //16
-            BACKGROUND_INTENSITY                                    //17
-    };
-
     WORD Win32Handler::getCurrentAttrs() {
         CONSOLE_SCREEN_BUFFER_INFO info;
         GetConsoleScreenBufferInfo(Win32Handler::outHandler, &info);
@@ -46,5 +23,7 @@ namespace cc {
             color_word |= Win32Handler::w32_attrs[(bg ? 17 : 16)];
         return color_word;
     }
+
+    constexpr WORD Win32Handler::w32_attrs[];
 
 }
